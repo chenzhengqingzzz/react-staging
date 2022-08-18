f3127069b9f94cbeee0d0b7c16f6fd65c0802295
陈正清 <tenchenzhengqing@qq.com>
2022-08-18 16:24:16 +0800

    代码提交

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 //创建“外壳组件”App
 import React, {Component} from "react";
+// 不指定文件就默认制定index.jsx
+import Hello from "./components/Hello";
+import Welcome from "./components/Welcome";
 // 从react身上获取Component
 // const {Component} = React
 
-class App extends Component{
+//创建并暴露App组件
+export default class App extends Component{
     render(){
         return (
             <div>
-                hello,react!
+                <Hello/>
+                <Welcome/>
             </div>
         )
     }
 }
-
-//默认暴露App组件
-export default App
