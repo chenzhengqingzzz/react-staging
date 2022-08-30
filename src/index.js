cc2345bd03fa0fc476a5fe93099e190defe7c81c
陈正清 <tenchenzhengqing@qq.com>
2022-08-30 21:50:45 +0800

    正确代理提交

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-//引入React核心库
-import React from "react"
+//引入react核心库
+import React from 'react'
 //引入ReactDOM
 import ReactDOM from 'react-dom'
-//引入App组件
-import App from './App.jsx'
+//引入App
+import App from './App'
 
-//渲染App组件到页面
-ReactDOM.render(<App/> , document.getElementById('root'))
+ReactDOM.render(<App/>,document.getElementById('root'))
