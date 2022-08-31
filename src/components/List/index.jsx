7c261fb89d69a6025cfa6ab2473c79394e951d86
陈正清 <tenchenzhengqing@qq.com>
2022-08-31 21:20:12 +0800

    代码提交

---
+++ src/components/List/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class List extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="card">
+            <a rel="noreferrer" href="https://github.com/快来和我击剑" target="_blank">
+            <img alt="head_portrait" src="https://syimg.3dmgame.com/uploadimg/upload/image/20201112/20201112175542_72765.gif" style={{width: '100px'}}/>
+            </a>
+            <p className="card-text">快来和我击剑</p>
+        </div>
+        <div className="card">
+            <a rel="noreferrer" href="https://github.com/快来和我击剑" target="_blank">
+            <img alt="head_portrait" src="https://syimg.3dmgame.com/uploadimg/upload/image/20201112/20201112175542_72765.gif" style={{width: '100px'}}/>
+            </a>
+            <p className="card-text">快来和我击剑</p>
+        </div>
+        <div className="card">
+            <a rel="noreferrer" href="https://github.com/快来和我击剑" target="_blank">
+            <img alt="head_portrait" src="https://syimg.3dmgame.com/uploadimg/upload/image/20201112/20201112175542_72765.gif" style={{width: '100px'}}/>
+            </a>
+            <p className="card-text">快来和我击剑</p>
+        </div>
+        <div className="card">
+            <a rel="noreferrer" href="https://github.com/快来和我击剑" target="_blank">
+            <img alt="head_portrait" src="https://syimg.3dmgame.com/uploadimg/upload/image/20201112/20201112175542_72765.gif" style={{width: '100px'}}/>
+            </a>
+            <p className="card-text">快来和我击剑</p>
+        </div>
+        <div className="card">
+            <a rel="noreferrer" href="https://github.com/快来和我击剑" target="_blank">
+            <img alt="head_portrait" src="https://syimg.3dmgame.com/uploadimg/upload/image/20201112/20201112175542_72765.gif" style={{width: '100px'}}/>
+            </a>
+            <p className="card-text">快来和我击剑</p>
+        </div>
+      </div>
+    )
+  }
+}
