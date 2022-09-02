46361b3214fae6e36eee317b58e142ec0a6e815c
陈正清 <tenchenzhengqing@qq.com>
2022-09-02 11:28:34 +0800

    看b站评论原先setupProxy有错 需要npm i http-proxy-middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,15 @@
-const proxy = require('http-proxy-middleware')
-
-module.exports = function(app){
-	app.use(
-		proxy('/api1',{ //遇见/api1前缀的请求，就会触发该代理配置
-			target:'http://localhost:5000', //请求转发给谁
-			changeOrigin:true,//控制服务器收到的请求头中Host的值
-			pathRewrite:{'^/api1':''} //重写请求路径(必须)
-		})
-	)
+const {createProxyMiddleware} = require('http-proxy-middleware')
+module.exports = function(app) {
+  app.use(
+    createProxyMiddleware('/api1',{ //遇见/apil前缀的请求就会触发
+      target: 'http://localhost:5000', //配置转发目标地址
+      changeOrigin: true, //控制服务器接收到的请求头中host字段的值
+      pathRewrite: {'^/api1': ''} //去除请求前缀址(必须配置)
+    }),
+    createProxyMiddleware('/api2', { 
+      target: 'http://localhost:5001', 
+      changeOrigin: true, 
+      pathRewrite: {'^/api1': ''}
+    })
+  )
 }
