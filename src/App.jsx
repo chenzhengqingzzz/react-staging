04f0d2a4ece6cd7d29354358c385715db46a59fc
陈正清 <tenchenzhengqing@qq.com>
2022-09-05 14:56:01 +0800

    代码提交

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home' //Home是路由组件
 import About from './pages/About' //About是路由组件
 import Header from './components/Header' //Header是一般组件
 import MyNavLink from './components/MyNavLink'
-import Test from './pages/Test'
 
 export default class App extends Component {
   render() {
@@ -28,8 +27,8 @@ export default class App extends Component {
               {/* <NavLink activeClassName="atguigu" className="list-group-item" to="home">Home</NavLink> */}
               {/* <MyNavLink to="/about">About</MyNavLink> */}
               {/* <MyNavLink to="/home">Home</MyNavLink> */}
-              <MyNavLink to="/about" children="About"/>
-              <MyNavLink to="/home" children="Home"/>
+              <MyNavLink to="/atguigu/about" children="About"/>
+              <MyNavLink to="/atguigu/home" children="Home"/>
           </div>
         </div>
         <div className="col-xs-6">
@@ -38,9 +37,8 @@ export default class App extends Component {
               {/* 注册路由 */}
               {/* 包裹Switch，选中则不会继续往下匹配路由，提高效率，节省资源 */}
               <Switch>
-                <Route path="/about" component={About}/>
-                <Route path="/home" component={Home}/>
-                <Route path="/home" component={Test}/>
+                <Route path="/atguigu/about" component={About}/>
+                <Route path="/atguigu/home" component={Home}/>
               </Switch>
             </div>
           </div>
