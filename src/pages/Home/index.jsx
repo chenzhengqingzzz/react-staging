77f6d0e777305d74d03446ade17739872d9df956
陈正清 <tenchenzhengqing@qq.com>
2022-09-05 19:48:30 +0800

    代码提交

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import MyNavLink from '../../components/MyNavLink'
+import Message from './Message'
+import News from './News'
 
 export default class Home extends Component {
   render() {
     return (
-      <h3>我是Home的内容</h3>
+    <div>
+      <h2>Home组件内容</h2>
+      <div>
+        <ul className="nav nav-tabs">
+          <li>
+            <MyNavLink to="/news" children="News"/>
+          </li>
+          <li>
+            <MyNavLink to="message" children="Message"/>
+          </li>
+        </ul>
+        {/* 注册路由 */}
+        <News/>
+        <Message/>
+      </div>
+    </div>
     )
   }
 }
