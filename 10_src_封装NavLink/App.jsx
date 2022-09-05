432270f70880211a107162afb8b19977ebe27b1b
陈正清 <tenchenzhengqing@qq.com>
2022-09-05 11:00:16 +0800

    代码提交

--- 10_src_封装NavLink/App.jsx
+++ 10_src_封装NavLink/App.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import {NavLink, Route, Switch} from 'react-router-dom'
+import {NavLink, Route} from 'react-router-dom'
 import Home from './pages/Home' //Home是路由组件
 import About from './pages/About' //About是路由组件
 import Header from './components/Header' //Header是一般组件
 import MyNavLink from './components/MyNavLink'
-import Test from './pages/Test'
 
 export default class App extends Component {
   render() {
@@ -36,12 +35,8 @@ export default class App extends Component {
           <div className="panel">
             <div className="panel-body">
               {/* 注册路由 */}
-              {/* 包裹Switch，选中则不会继续往下匹配路由，提高效率，节省资源 */}
-              <Switch>
                 <Route path="/about" component={About}/>
                 <Route path="/home" component={Home}/>
-                <Route path="/home" component={Test}/>
-              </Switch>
             </div>
           </div>
         </div>
