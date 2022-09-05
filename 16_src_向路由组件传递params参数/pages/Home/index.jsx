c9ff35d93ab853c18c53dea658cdb56e5d068b31
陈正清macbook pro <tenchenzhengqing@qq.com>
2022-09-06 01:22:42 +0800

    向路由组件传递params参数

---
+++ 16_src_向路由组件传递params参数/pages/Home/index.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import MyNavLink from '../../components/MyNavLink'
+import Message from './Message'
+import News from './News'
+
+export default class Home extends Component {
+  render() {
+    return (
+    <div>
+      <h2>Home组件内容</h2>
+      <div>
+        <ul className="nav nav-tabs">
+          <li>
+            <MyNavLink to="/home/news" children="News"/>
+          </li>
+          <li>
+            <MyNavLink to="/home/message" children="Message"/>
+          </li>
+        </ul>
+        {/* 注册路由 */}
+        <Switch>
+          <Route path="/home/news" component={News}/>
+          <Route path="/home/message" component={Message}/>
+          <Redirect to="/home/news"/>
+        </Switch>
+      </div>
+    </div>
+    )
+  }
+}
