678fb5dfadcf3aef4e691e32697fa7958cfc070b
陈正清 <tenchenzhengqing@qq.com>
2022-09-06 16:47:20 +0800

    向路由组件传递search参数

---
+++ 17_src_向路由组件传递search参数/components/MyNavLink/index.jsx
@@ -0,0 +1,16 @@
+import React, { children, Component } from 'react'
+import { NavLink } from 'react-router-dom'
+
+export default class MyNavLink extends Component {
+  render() {
+    // console.log(this.props);
+    // const {children} = this.props
+    return (
+        // <NavLink activeClassName="atguigu" className="list-group-item" {...this.props}>{children}</NavLink>
+        // 标签体内容是一个特殊的标签属性，可以直接用props传递，其对应事例为children
+        <NavLink activeClassName="atguigu" className="list-group-item" {...this.props}/>
+
+    )
+  }
+}
+
