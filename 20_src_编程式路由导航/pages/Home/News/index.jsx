a0703eb195c56e4e76976698b36ecd0ebbc0d2bd
陈正清 <tenchenzhengqing@qq.com>
2022-09-09 12:28:20 +0800

    Route5最后相关代码提交

--- 20_src_编程式路由导航/pages/Home/News/index.jsx
+++ 20_src_编程式路由导航/pages/Home/News/index.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export default class News extends Component {
+
+  // componentDidMount = () => {
+  //   setInterval(() => {
+  //     this.props.history.push(`/home/message`)
+  //   }, 2000)
+  // } 
+
   render() {
     return (
         <ul>
