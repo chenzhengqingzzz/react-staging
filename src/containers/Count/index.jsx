c372454acbd924be735e45b7a717b7199c170793
陈正清 <tenchenzhengqing@qq.com>
2022-09-13 21:59:02 +0800

    代码提交

--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -1,9 +1,16 @@
 //引入Count的UI组件
 import CountUI from '../../components/Count'
-//引入redux里的store
-import store from '../../redux/store'
 //引入connect由于连接UI组件与redux
 import { connect } from 'react-redux'
+
+//a函数的返回值作为状态传递给了UI组件
+function a() {
+    
+}
+
+function b() {
+    
+}
 
 //使用connect()()创建一个Count的容器组件
 const CountContainer = connect()(CountUI)
