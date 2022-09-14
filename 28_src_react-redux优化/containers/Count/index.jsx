397a7db7f5afb89ad91ab58818f8726ba5e65c3f
陈正清macbook pro <tenchenzhengqing@qq.com>
2022-09-15 01:24:19 +0800

    react-redux优化

--- 28_src_react-redux优化/containers/Count/index.jsx
+++ 28_src_react-redux优化/containers/Count/index.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
+//引入action
+import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from '../../redux/count_action'
+//引入connect由于连接UI组件与redux
+import { connect } from 'react-redux'
 
-export default class Count extends Component {
+//定义UI组件
+class Count extends Component {
 
     //state可以又自己的状态，只供自己使用不交给redux
     state = {carName: '奔驰c63'}
@@ -53,3 +58,37 @@ export default class Count extends Component {
     )
   }
 }
+
+//映射状态
+const mapStateToProps = (state) => {
+    return {count: state}
+}
+
+//映射操作状态的方法
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+        jia: (number) => {
+            dispatch(createIncrementAction(number))
+        },
+        
+        jian: (number) => {
+            dispatch(createDecrementAction(number))
+        },
+
+        yibujia: (number, time) => {
+            dispatch(createIncrementAsyncAction(number, time))
+        }
+    }
+}
+/*
+    mapDispatchToProps这个函数可这样简写(写成对象):
+    jia: number => dispatch(createIncrementAction(number)),
+    jian: number => dispatch(createDecrementAction(number)),
+    yibujia: (number, time) => dispatch(createIncrementAsyncAction(number, time)),
+*/
+
+//使用connect()()创建一个Count的容器组件
+const CountContainer = connect(mapStateToProps, mapDispatchToProps)(Count)
+
+export default CountContainer
--- src/containers/Count/index.jsx
+++ src/containers/Count/index.jsx
@@ -1,9 +1,63 @@
-//引入Count的UI组件
-import CountUI from '../../components/Count'
+import React, { Component } from 'react'
 //引入action
 import { createIncrementAction, createDecrementAction, createIncrementAsyncAction } from '../../redux/count_action'
 //引入connect由于连接UI组件与redux
 import { connect } from 'react-redux'
+
+//定义UI组件
+class Count extends Component {
+
+    //state可以又自己的状态，只供自己使用不交给redux
+    state = {carName: '奔驰c63'}
+
+    //加法
+    increment = () => {
+        //获取用户输入
+        const {value} = this.selectedNumber
+        this.props.jia(value * 1)
+    }
+
+    //减法
+    decrement = () => {
+        //获取用户输入
+        const {value} = this.selectedNumber
+        this.props.jian(value * 1)
+    }
+
+    //奇数再加
+    incrementIfOdd = () => {
+        //获取用户输入
+        const {value} = this.selectedNumber
+        if (this.props.count %2 !== 0) {
+          this.props.yibujia(value * 1)
+        }
+    }
+
+    //异步加
+    incrementAsync = () => {
+        //获取用户输入
+        const {value} = this.selectedNumber
+        this.props.yibujia(value * 1, 500)
+    }
+
+  render() {
+    console.log('UI组件接收到的props-->', this.props);
+    return (
+      <div>
+        <h1>当前求和为:{this.props.count}</h1>
+        <select ref={c => this.selectedNumber = c}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select>&nbsp;
+        <button onClick={this.increment}>+</button>&nbsp;
+        <button onClick={this.decrement}>-</button>&nbsp;
+        <button onClick={this.incrementIfOdd}>当前求和为奇数再加</button>&nbsp;
+        <button onClick={this.incrementAsync}>异步加</button>&nbsp;
+      </div>
+    )
+  }
+}
 
 //映射状态
 const mapStateToProps = (state) => {
@@ -35,6 +89,6 @@ const mapDispatchToProps = (dispatch) => {
 */
 
 //使用connect()()创建一个Count的容器组件
-const CountContainer = connect(mapStateToProps, mapDispatchToProps)(CountUI)
+const CountContainer = connect(mapStateToProps, mapDispatchToProps)(Count)
 
 export default CountContainer
